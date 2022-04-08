c7911a3135867c6643b0064f549d1a631f6ccb71
Ivan Pyatlin <piatlin2002@gmail.com>
2022-04-08 22:39:59 +0300

    performance upgrade !

--- src/new_solve.py
+++ src/new_solve.py
@@ -9,22 +9,26 @@ def pars_file(path):
     cnf = all_cnf.split("\n")
 
     for i in range(len(cnf)):
-        if cnf[i][-1] == "0":
-            cnf[i] = list(map(int, cnf[i][:-1].split()))
+        new_line = cnf[i].split()
+        if new_line[-1] == "0":
+            cnf[i] = list(map(int, new_line[:-1]))
         else:
-            cnf[i] = list(map(int, cnf[i][:].split()))
+            cnf[i] = list(map(int, new_line))
     return cnf
 
 
 class Cnf:
-    def __init__(self, cnf_: List[List[int]], solve=None):
+    def __init__(self, cnf_: List[List[int]], solve=None, uniq_literals=None):
         if solve is None:
             solve = []
         self.solve = solve
         self.cluster_list = cnf_
+        self.uniq_literals = self.get_uniq_literals()
+        self.is_bad_solve = False
+        if uniq_literals is None:
+            uniq_literals = self.get_uniq_literals()
+        self.uniq_literals = uniq_literals
 
-    def __str__(self):
-        return self.cluster_list.__str__()
 
     def get_uniq_literals(self):
         return set([literal for cluster in self.cluster_list for literal in cluster])
@@ -36,30 +40,33 @@ class Cnf:
         return self.cluster_list == []
 
     def pure_literal_assign(self):
-        literals_to_check = self.get_uniq_literals()
-        while len(literals_to_check) != 0:
-            literal = literals_to_check.pop()
-            if -literal not in literals_to_check:  # означает, что перед нами "чистый" литерал
+        literals_to_check = iter(self.uniq_literals.copy())
+        literal = next(literals_to_check, None)
+        while literal is not None:
+            if -literal not in self.uniq_literals:  # означает, что перед нами "чистый" литерал
                 self.solve.append(literal)
+                self.uniq_literals.remove(literal)
                 self.cluster_list = list(filter(lambda cluster: literal not in cluster, self.cluster_list))
-                literals_to_check = self.get_uniq_literals()
-            else:
-                literals_to_check.remove(-literal)
+            literal = next(literals_to_check, None)
 
     def unit_propagate(self):
         i = 0
         while i < len(self.cluster_list):
-            if len(self.cluster_list[i]) == 1:
+            le = len(self.cluster_list[i])
+            if le == 1:
                 literal = self.cluster_list[i][0]
                 self.solve.append(literal)
-                # удаляем все кластера где есть литерал из единичного клоза
-                self.cluster_list = self.use_new_literal(literal)
+                self.uniq_literals.remove(literal)
+                self.cluster_list = self.use_new_literal(literal) # удаляем все кластера где есть литерал из единичного клоза
                 i = 0
+            elif le == 0:
+                self.is_bad_solve = True
+                break
             else:
                 i += 1
 
     def use_new_literal(self, literal) -> List[List[int]]:
-        new_cnf = list(filter(lambda cluster: literal not in cluster, self.cluster_list))
+        new_cnf = filter(lambda cluster: literal not in cluster, self.cluster_list)
         new_cnf = list(map(
             lambda cluster: list(filter(lambda lit: lit != -literal, cluster)), new_cnf
         ))
@@ -71,14 +78,14 @@ class Cnf:
                Cnf(self.use_new_literal(-literal), self.solve + [-literal])
 
     def get_solution(self):
-        if self.solve_ready(): return sorted(self.solve, key=abs)
         if self.bad_solve(): return []
+        if self.solve_ready(): return sorted(self.solve, key=abs)
 
         self.pure_literal_assign()
         self.unit_propagate()
 
+        if self.is_bad_solve: return []
         if self.solve_ready(): return sorted(self.solve, key=abs)
-        if self.bad_solve(): return []
 
         cnf1, cnf2 = self.next_cnf()
 
--- test/unit_tests.py
+++ test/unit_tests.py
@@ -19,8 +19,8 @@ class EasyTestCases(unittest.TestCase):
         )
 
     def test_simple3(self):
-        self.assertEqual(
-            Cnf(pars_file("../input/input3.txt")).get_solution(), [1, 2]
+        self.assertIsNot(
+            Cnf(pars_file("../input/input3.txt")).get_solution(), []
         )
 
 
@@ -38,6 +38,11 @@ class HardTestCases(unittest.TestCase):
     def test_hard3(self):
         self.assertIsNot(
             Cnf(pars_file("../input/input6.txt")).get_solution(), []
+        )
+
+    def test_hard4(self):
+        self.assertEqual(
+            Cnf(pars_file("../input/input7.txt")).get_solution(), []
         )
 
 
