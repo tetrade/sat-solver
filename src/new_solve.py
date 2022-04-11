e0164f61645d44abd2cfafbd1755da118a2d97e7
Ivan Pyatlin <piatlin2002@gmail.com>
2022-04-11 22:59:29 +0300

    performance upg + uniform tests

--- src/new_solve.py
+++ src/new_solve.py
@@ -6,14 +6,17 @@ def pars_file(path):
     with open(path) as f:
         all_cnf = f.read()
 
-    cnf = all_cnf.split("\n")
+    file = all_cnf.split("\n")
+    cnf = []
 
-    for i in range(len(cnf)):
-        new_line = cnf[i].split()
+    for i in range(len(file)):
+        if file[i][0] == 'c' or file[i][0] == 'p': continue
+        if file[i][0] == '%': break
+        new_line = file[i].split()
         if new_line[-1] == "0":
-            cnf[i] = list(map(int, new_line[:-1]))
+            cnf.append(list(map(int, new_line[:-1])))
         else:
-            cnf[i] = list(map(int, new_line))
+            cnf.append(list(map(int, new_line)))
     return cnf
 
 
@@ -23,18 +26,13 @@ class Cnf:
             solve = []
         self.solve = solve
         self.cluster_list = cnf_
-        self.uniq_literals = self.get_uniq_literals()
         self.is_bad_solve = False
         if uniq_literals is None:
             uniq_literals = self.get_uniq_literals()
         self.uniq_literals = uniq_literals
 
-
     def get_uniq_literals(self):
         return set([literal for cluster in self.cluster_list for literal in cluster])
-
-    def bad_solve(self) -> bool:
-        return self.cluster_list.count([]) > 0
 
     def solve_ready(self) -> bool:
         return self.cluster_list == []
@@ -57,7 +55,8 @@ class Cnf:
                 literal = self.cluster_list[i][0]
                 self.solve.append(literal)
                 self.uniq_literals.remove(literal)
-                self.cluster_list = self.use_new_literal(literal) # удаляем все кластера где есть литерал из единичного клоза
+                self.cluster_list = self.use_new_literal(
+                    literal)  # удаляем все кластера где есть литерал из единичного клоза
                 i = 0
             elif le == 0:
                 self.is_bad_solve = True
@@ -74,12 +73,10 @@ class Cnf:
 
     def next_cnf(self):
         literal = choice(choice(self.cluster_list))
-        return Cnf(self.use_new_literal(literal), self.solve + [literal]), \
-               Cnf(self.use_new_literal(-literal), self.solve + [-literal])
+        return Cnf(self.use_new_literal(literal), self.solve + [literal], self.uniq_literals.copy()), \
+               Cnf(self.use_new_literal(-literal), self.solve + [-literal], self.uniq_literals)
 
     def get_solution(self):
-        if self.bad_solve(): return []
-        if self.solve_ready(): return sorted(self.solve, key=abs)
 
         self.pure_literal_assign()
         self.unit_propagate()
--- test/unit_tests.py
+++ test/unit_tests.py
@@ -1,5 +1,6 @@
 import unittest
 from src.new_solve import *
+import os
 
 
 class EasyTestCases(unittest.TestCase):
@@ -9,34 +10,34 @@ class EasyTestCases(unittest.TestCase):
         )
 
     def test_simple1(self):
-        self.assertIsNot(
+        self.assertNotEqual(
             Cnf(pars_file("../input/input1.txt")).get_solution(), []
         )
 
     def test_simple2(self):
-        self.assertIsNot(
+        self.assertNotEqual(
             Cnf(pars_file("../input/input2.txt")).get_solution(), []
         )
 
     def test_simple3(self):
-        self.assertIsNot(
+        self.assertNotEqual(
             Cnf(pars_file("../input/input3.txt")).get_solution(), []
         )
 
 
 class HardTestCases(unittest.TestCase):
     def test_hard1(self):
-        self.assertIsNot(
+        self.assertNotEqual(
             Cnf(pars_file("../input/input4.txt")).get_solution(), []
         )
 
     def test_hard2(self):
-        self.assertIsNot(
+        self.assertNotEqual(
             Cnf(pars_file("../input/input5.txt")).get_solution(), []
         )
 
     def test_hard3(self):
-        self.assertIsNot(
+        self.assertNotEqual(
             Cnf(pars_file("../input/input6.txt")).get_solution(), []
         )
 
@@ -44,6 +45,20 @@ class HardTestCases(unittest.TestCase):
         self.assertEqual(
             Cnf(pars_file("../input/input7.txt")).get_solution(), []
         )
+
+
+class UniformTestCases(unittest.TestCase):
+    def test_uniform_all_sat(self):
+        for filename in os.listdir('../uniform_test/sat'):
+            self.assertNotEqual(
+                Cnf(pars_file(os.path.join('../uniform_test/sat', filename))).get_solution(), []
+            )
+
+    def test_uniform_all_unsat(self):
+        for filename in os.listdir('../uniform_test/unsat'):
+            self.assertEqual(
+                Cnf(pars_file(os.path.join('../uniform_test/unsat', filename))).get_solution(), []
+            )
 
 
 if __name__ == '__main__':
